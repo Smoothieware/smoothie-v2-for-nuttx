f05d695c9655808c4536e11afae54083cc4cfe22
Jim Morris <morris@wolfman.com>
2017-06-28 02:48:57 -0700

    add flash read timing,and increse stepticker to 50Khz, buit requires -O2 and spifi clock doubled

--- Firmware/TestUnits/TEST_time-armsolution.cpp
+++ Firmware/TestUnits/TEST_time-armsolution.cpp
@@ -20,7 +20,7 @@ REGISTER_TEST(TimeTest, delta_ik)
     ActuatorCoordinates ac;
     BaseSolution* k= new LinearDeltaSolution(cr);
 
-    uint32_t n= 10000000;
+    uint32_t n= 100000;
     systime_t st = clock_systimer();
 
     for(uint32_t i=0;i<n;i++) k->cartesian_to_actuator( millimeters, ac);
@@ -31,4 +31,17 @@ REGISTER_TEST(TimeTest, delta_ik)
     delete k;
 
     TEST_PASS();
+}
+
+REGISTER_TEST(TimeTest, read_flash)
+{
+    uint32_t *p = (uint32_t *)0x14000000;
+
+    uint32_t n= 8000000;
+    systime_t st = clock_systimer();
+    while(p < (uint32_t *)(0x14000000+(n/4))) {
+        uint32_t c= *p++;
+    }
+    systime_t en = clock_systimer();
+    printf("elapsed time %d us over %d bytes %1.4f mb/sec\n", TICK2USEC(en-st), n, (float)n/TICK2USEC(en-st));
 }
