ea1c08a32b8752ea823b1504cf3d18195fb0ec2e
Jim Morris <morris@wolfman.com>
2017-09-23 11:44:33 +0100

    create HAL Adc class

--- Firmware/TestUnits/TEST_pwm.cpp
+++ Firmware/TestUnits/TEST_pwm.cpp
@@ -42,132 +42,18 @@ REGISTER_TEST(PWMTest, basic)
     Chip_SCTPWM_Start(SCT_PWM);
 }
 
-#if 1
-#include <string>
-#include <cstring>
-#include <cctype>
-
-#include <tuple>
-#include <vector>
-/* 43xx Pinmap for PWM to CTOUT and function
-Pin  a, b, COUT#, Function
-*/
-static std::vector<std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>> lut {
-    {0x01, 1,  7,  1},
-    {0x01, 2,  6,  1},
-    {0x01, 3,  8,  1},
-    {0x01, 4,  9,  1},
-    {0x01, 5,  10, 1},
-    {0x01, 7,  13, 2},
-    {0x01, 8,  12, 2},
-    {0x01, 9,  11, 2},
-    {0x01, 10, 14, 2},
-    {0x01, 11, 15, 2},
-    {0x02, 7,  1,  1},
-    {0x02, 8,  0,  1},
-    {0x02, 9,  3,  1},
-    {0x02, 10, 2,  1},
-    {0x02, 11, 5,  1},
-    {0x02, 12, 4,  1},
-    {0x04, 1,  1,  1},
-    {0x04, 2,  0,  1},
-    {0x04, 3,  3,  1},
-    {0x04, 4,  2,  1},
-    {0x04, 5,  5,  1},
-    {0x04, 6,  4,  1},
-    {0x06, 5,  6,  1},
-    {0x06, 12, 7,  1},
-    {0x07, 0,  14, 1},
-    {0x07, 1,  15, 1},
-    {0x07, 4,  13, 1},
-    {0x07, 5,  12, 1},
-    {0x07, 6,  11, 1},
-    {0x07, 7,  8,  1},
-    {0x0A, 4,  9,  1},
-    {0x0B, 0,  10, 1},
-    {0x0B, 1,  6,  5},
-    {0x0B, 2,  7,  5},
-    {0x0B, 3,  8,  5},
-    {0x0D, 0,  15, 1},
-    {0x0D, 2,  7,  1},
-    {0x0D, 3,  6,  1},
-    {0x0D, 4,  8,  1},
-    {0x0D, 5,  9,  1},
-    {0x0D, 6,  10, 1},
-    {0x0D, 9,  13, 1},
-    {0x0D, 11, 14, 6},
-    {0x0D, 12, 10, 6},
-    {0x0D, 13, 13, 6},
-    {0x0D, 14, 11, 6},
-    {0x0D, 15, 8,  6},
-    {0x0D, 16, 12, 6},
-    {0x0E, 5,  3,  1},
-    {0x0E, 6,  2,  1},
-    {0x0E, 7,  5,  1},
-    {0x0E, 8,  4,  1},
-    {0x0E, 11, 12, 1},
-    {0x0E, 12, 11, 1},
-    {0x0E, 13, 14, 1},
-    {0x0E, 15, 0,  1},
-    {0x0F, 9,  1,  2}
-};
-
-static bool lookup_pin(uint8_t port, uint8_t pin, uint8_t& ctout, uint8_t& func)
+#include "Pwm.h"
+REGISTER_TEST(PWMTest, from_string)
 {
-    for(auto &p : lut) {
-        if(port == std::get<0>(p) && pin == std::get<1>(p)) {
-            ctout= std::get<2>(p);
-            func= std::get<3>(p);
-            return true;
-        }
-    }
+    Pwm pwm1;
+    TEST_ASSERT_FALSE(pwm1.is_valid());
+    TEST_ASSERT_FALSE(pwm1.from_string("X1.2"));
+    TEST_ASSERT_FALSE(pwm1.is_valid());
+    TEST_ASSERT_TRUE(pwm1.from_string("P2.12"));
+    TEST_ASSERT_TRUE(pwm1.is_valid());
 
-    return false;
-}
-
-static int pwm_index= 1;
-static int map_pin_to_pwm(const char *name)
-{
-    // specify pin name P1.6 and check it is mappable to a PWM channel
-    if(tolower(name[0]) == 'p') {
-        // pin specification
-        std::string str(name);
-        uint16_t port = strtol(str.substr(1).c_str(), nullptr, 16);
-        size_t pos = str.find_first_of("._", 1);
-        if(pos == std::string::npos) return 0;
-        uint16_t pin = strtol(str.substr(pos + 1).c_str(), nullptr, 10);
-
-        // now map to a PWM output
-        uint8_t ctout, func;
-        if(!lookup_pin(port, pin, ctout, func)) {
-            return 0;
-        }
-
-        // check if ctoun is already in use
-        // TODO
-
-        // setup pin for the PWM function
-        Chip_SCU_PinMuxSet(port, pin, func);
-
-        // TODO index is incremented for each pin
-        Chip_SCTPWM_SetOutPin(SCT_PWM, pwm_index, ctout);
-
-        return pwm_index++;
-    }
-
-    return 0;
-}
-
-REGISTER_TEST(PWMTest, map_pin)
-{
-    uint8_t ctout, func;
-    TEST_ASSERT_TRUE(lookup_pin(2, 11, ctout, func));
-    TEST_ASSERT_EQUAL_INT(ctout, 5);
-    TEST_ASSERT_EQUAL_INT(func, 1);
-
-    TEST_ASSERT_FALSE(lookup_pin(2, 13, ctout, func));
-    TEST_ASSERT_EQUAL_INT(map_pin_to_pwm("X1.2"), 0);
-    TEST_ASSERT_EQUAL_INT(map_pin_to_pwm("P2.12"), 1);
+    Pwm pwm2("P2.11");
+    TEST_ASSERT_TRUE(pwm2.is_valid());
 }
 
 
@@ -177,25 +63,23 @@ REGISTER_TEST(PWMTest, set_current)
     // set X driver to 400mA
     // set Y driver to 1amp
     // set Z driver to 1.5amp
-    int xind= map_pin_to_pwm("P7.4"); // X
-    TEST_ASSERT_TRUE(xind > 0);
+    Pwm pwmx("P7.4"); // X
+    TEST_ASSERT_TRUE(pwmx.is_valid());
     // dutycycle= current/2.0625
-    // TODO we need to calculate ticks ourselves as uint8 percentage is not very accurate
-    uint8_t dcp= floorf((0.4F*100)/2.0625F);
-    Chip_SCTPWM_SetDutyCycle(SCT_PWM, xind, Chip_SCTPWM_PercentageToTicks(SCT_PWM, dcp));
+    float dcp= 0.4F/2.0625F;
+    pwmx.set(dcp);
 
-    int yind= map_pin_to_pwm("PB.2"); // Y
-    TEST_ASSERT_TRUE(yind > xind);
+    Pwm pwmy("PB.2"); // Y
+    TEST_ASSERT_TRUE(pwmy.is_valid());
 
     // dutycycle= current/2.0625
-    dcp= floorf((1.0F*100)/2.0625F);
-    Chip_SCTPWM_SetDutyCycle(SCT_PWM, yind, Chip_SCTPWM_PercentageToTicks(SCT_PWM, dcp));
+    dcp= 1.0F/2.0625F;
+    pwmy.set(dcp);
 
-    int zind= map_pin_to_pwm("PB.3"); // Z
-    TEST_ASSERT_TRUE(zind > yind);
+    Pwm pwmz("PB.3"); // Z
+    TEST_ASSERT_TRUE(pwmz.is_valid());
     // dutycycle= current/2.0625
-    dcp= floorf((1.5F*100)/2.0625F);
-    Chip_SCTPWM_SetDutyCycle(SCT_PWM, zind, Chip_SCTPWM_PercentageToTicks(SCT_PWM, dcp));
-
+    dcp= 1.5F/2.0625F;
+    pwmz.set(dcp);
 }
-#endif
+
---
+++ Firmware/src/HAL/Pwm.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+
+class Pwm
+{
+public:
+	Pwm();
+	~Pwm(){};
+	Pwm(const char* pin);
+	bool from_string(const char *pin);
+	bool is_valid() const { return valid; }
+	// set duty cycle 0-1
+	void set(float v);
+
+private:
+	bool lookup_pin(uint8_t port, uint8_t pin, uint8_t& ctout, uint8_t& func);
+	int map_pin_to_pwm(const char *name);
+
+	static int pwm_index;
+	bool valid{false};
+	uint8_t index;
+
+};
